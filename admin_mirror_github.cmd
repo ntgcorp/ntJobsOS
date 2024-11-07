4e30ae00814306f246f150d342bb37f3a273f109
ntgcorp <ntgcorp@gmail.com>
2024-11-07 16:17:50 +0100

    Deleted files not public

--- admin_mirror_github.cmd
+++
@@ -1,7 +0,0 @@
-
-REM CHANGE WITH PATH FOR YOUR PC
-SET GDRIVE=M:
-IF "%COMPUTERNAME%"=="ALIENWREVI" SET GDRIVE=G:
-IF "%COMPUTERNAME%"=="ACERMINI" SET GDRIVE=G:
-
-admin_mirror "%GDRIVE%\Il mio Drive\Progetti\ntJobsPy\" "%GDRIVE%\Il mio Drive\Github\ntJobsOS\" admin_mirror_github.txt
